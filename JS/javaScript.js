4c001a48cbb98a454fed8484de3cd1b22db26a60
David Moreno <morenocapeldavid@gmail.com>
2020-03-09 16:00:42 +0100

    Add all

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -1,5 +1,5 @@
 const seccionesPagina = new fullpage('#fullpage', {
-    
+
          autoScrolling: true, // Se activa el scroll.
 		 fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
 		 fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
@@ -7,5 +7,19 @@ const seccionesPagina = new fullpage('#fullpage', {
 		 scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
 		 css3: true, // Si usara CSS3 o javascript.
 		 easingcss3: 'ease-out', // Curva de velocidad del efecto.
-		 loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
-});
+		 loopBottom: true, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+
+         // ──────────────────────────────────────────────────
+	//   :::::: Barra de navegación
+	// ──────────────────────────────────────────────────
+		 navigation: true, // Muesta la barra de navegación.
+		 menu: '#menu', // Menu de navegación.
+		 anchors: ['inicio', 'que-es', 'contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+		 navigationTooltips: ['MY PUB', '¿QUE ES?', 'RRSS'], // Tooltips que mostrara por cada boton.
+		 showActiveTooltip: false, // Mostrar tooltip activa.
+
+         //   :::::: Secciones
+	// ──────────────────────────────────────────────────
+		 sectionsColor : ['#000', '#c2c2c2', '#000'], // Color de fondo de cada seccion.
+		 verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
+        });
